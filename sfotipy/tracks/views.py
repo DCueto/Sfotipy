2a3a9d23642c8a478a62a8f3c09fa93e83bd1360
Daniel Cueto <dcueto14.info@gmail.com>
2014-12-11 22:33:23 +0100

    Rendering data models in views w/ render function

--- sfotipy/tracks/views.py
+++ sfotipy/tracks/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+
+from .models import Track
 
 # Create your views here.
 
-def track_view(request):
-	return HttpResponse("Ok")
+def track_view(request, title):
+
+	'''	try:
+		track = Track.objects.get(title=title)
+	except Track.DoesNotExist:
+		raise Http404 '''
+
+	track = get_object_or_404(Track, title=title) 
+	bio = track.artist.biography
+
+
+
+	return render(request, 'track.html', {'track': track, 'bio': bio})
